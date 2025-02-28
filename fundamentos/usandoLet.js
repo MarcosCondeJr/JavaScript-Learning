00b680ab3a73845d3c2fa761977b99aff4a3ceef
marcoscondejr <marcos14conde@hotmail.com>
2025-02-27 23:11:28 -0300

    diferencas var e let

---
+++ fundamentos/usandoLet.js
@@ -0,0 +1,8 @@
+let numero = 1
+{
+    let numero = 2
+    console.log('dentro = ',numero)
+}
+console.log('fora = ', numero)
+
+//A diferença do let é que se dentro de algum escopo eu alterar o valor, fora dele ele não vai ser alterado
