dd4d42c0284276ec5db8a6f5c1e29abf65b5ecae
marcoscondejr <marcos14conde@hotmail.com>
2025-02-28 00:02:59 -0300

    notacaoPonto

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,16 @@
+console.log('a = ', a)
+
+var a = 'teste'
+
+console.log('a = ', a)
+
+// Ele faz o içamento, ou joga para cima, o resultado do primeiro 
+// sai como undefined pois é como se fosse uma variavel se declaração
+
+console.log('b = ', b)
+
+let b = 'teste'
+
+console.log('a = ', b)
+
+//Ja com o let ele vai estourar ja no primeito console
---
+++ fundamentos/objetos2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+//O primeiro retorna function e o segundo retorna um object pq estou istanciando ele
+
+class Produto {}
+console.log(typeof new Produto)
+
+const Cliente = function () {}
+console.log(typeof Cliente)
